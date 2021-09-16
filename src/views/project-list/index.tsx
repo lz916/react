6cbe9426f745b9b500ea7917393d74ff851c7c64
liaozhen <zhen.liao@baobeituan.com>
2021-09-16 17:45:29 +0800

    完成列表和ts类型检测

---
+++ src/views/project-list/index.tsx
@@ -0,0 +1,43 @@
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { useState, useEffect } from "react";
+import { cleanObject, useDebounce } from "../../utils";
+import qs from "qs";
+import { useMount } from "../../utils";
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+  const debouncedParam = useDebounce(param, 2000);
+  const apiUrl = process.env.REACT_APP_API_URL;
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+  return (
+    <div>
+      <SearchPanel
+        param={param}
+        setParam={setParam}
+        users={users}
+      ></SearchPanel>
+      <List list={list} users={users}></List>
+    </div>
+  );
+};
